ccb3deb867e920443f78eabdb8855002c4d59765
Keegan H <keeganslane@gmail.com>
2019-05-30 20:54:06 -0700

    bg stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Particles from 'react-particles-js';
 
 function App() {
   return (
     <div className="App">
       <header className="header">
+        
         <img src={logo} className="logo" alt="logo" />
+        <Particles
+    params={{
+	    "particles": {
+	        "number": {
+	            "value": 600,
+	            "density": {
+	                "enable": true,
+	                "value_area": 1500
+	            }
+	        },
+	        "line_linked": {
+	            "enable": true,
+	            "opacity": 0.02
+	        },
+	        "move": {
+	            "direction": "right",
+	            "speed": 0.05
+	        },
+	        "size": {
+	            "value": 1
+	        },
+	        "opacity": {
+	            "anim": {
+	                "enable": true,
+	                "speed": 1,
+	                "opacity_min": 0.05
+	            }
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onclick": {
+	                "enable": true,
+	                "mode": "push"
+	            }
+	        },
+	        "modes": {
+	            "push": {
+	                "particles_nb": 1
+	            }
+	        }
+	    },
+	    "retina_detect": true
+	}} />
         <h1>
         Hello, <br ></br>
         I'm Keegan Helmbrecht.
