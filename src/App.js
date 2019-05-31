dc28975942998cefd447c89ac330e38bba8eae73
Keegan H <keeganslane@gmail.com>
2019-05-30 21:05:12 -0700

    formatting

--- src/App.js
+++ src/App.js
@@ -7,62 +7,62 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-        
+
         <img src={logo} className="logo" alt="logo" />
         <Particles
-    params={{
-	    "particles": {
-	        "number": {
-	            "value": 600,
-	            "density": {
-	                "enable": true,
-	                "value_area": 1500
-	            }
-	        },
-	        "line_linked": {
-	            "enable": true,
-	            "opacity": 0.02
-	        },
-	        "move": {
-	            "direction": "right",
-	            "speed": 0.05
-	        },
-	        "size": {
-	            "value": 1
-	        },
-	        "opacity": {
-	            "anim": {
-	                "enable": true,
-	                "speed": 1,
-	                "opacity_min": 0.05
-	            }
-	        }
-	    },
-	    "interactivity": {
-	        "events": {
-	            "onclick": {
-	                "enable": true,
-	                "mode": "push"
-	            }
-	        },
-	        "modes": {
-	            "push": {
-	                "particles_nb": 1
-	            }
-	        }
-	    },
-	    "retina_detect": true
-	}} />
+          params={{
+            "particles": {
+              "number": {
+                "value": 600,
+                "density": {
+                  "enable": true,
+                  "value_area": 1500
+                }
+              },
+              "line_linked": {
+                "enable": true,
+                "opacity": 0.02
+              },
+              "move": {
+                "direction": "right",
+                "speed": 0.05
+              },
+              "size": {
+                "value": 1
+              },
+              "opacity": {
+                "anim": {
+                  "enable": true,
+                  "speed": 1,
+                  "opacity_min": 0.05
+                }
+              }
+            },
+            "interactivity": {
+              "events": {
+                "onclick": {
+                  "enable": true,
+                  "mode": "push"
+                }
+              },
+              "modes": {
+                "push": {
+                  "particles_nb": 1
+                }
+              }
+            },
+            "retina_detect": true
+          }} />
         <h1>
-        Hello, <br ></br>
-        I'm Keegan Helmbrecht.
+          Hello, <br ></br>
+          I'm Keegan Helmbrecht.
         </h1>
         <div className="sub">
-        I'm a full stack developer living in the Bay Area.
+          I'm a full stack developer living in the Bay Area.
         </div>
-        <div className ="p">
-        Technical Expertise: <br ></br>
-        Python - React.js - JavaScript (ES6) - HTML / CSS - Django - Postgres SQL - Bash - Git - Pipenv - React Router - FlexBox & Grid - BEM - MVC - OOP - API engineering - ORM - OOP - Static Site Generation - SPA (Single Page Apps) - Webpack - Test Driven Development - Bootstrap - Agile
+        <div className="p">
+          Technical Expertise: <br ></br>
+          Python - React.js - JavaScript (ES6) - HTML / CSS - Django - Postgres SQL - Bash - Git - Pipenv - React Router - FlexBox & Grid - BEM - MVC - OOP - API engineering - ORM - OOP - Static Site Generation - SPA (Single Page Apps) - Webpack - Test Driven Development - Bootstrap - Agile
         </div>
         <a
           className="link"
