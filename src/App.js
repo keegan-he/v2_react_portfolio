0cc596b4c3daf0a46f2adde9334670e3590a6bc4
Keegan H <keeganslane@gmail.com>
2019-05-31 21:53:33 -0700

    practice practice practice

--- src/App.js
+++ src/App.js
@@ -6,52 +6,7 @@ function App() {
 	return (
 		<div className="App">
 			<header className="header">
-
 				<div className="part">
-					<Particles
-						params={{
-							"particles": {
-								"number": {
-									"value": 60,
-									"density": {
-										"enable": true,
-										"value_area": 1500
-									}
-								},
-								"line_linked": {
-									"enable": true,
-									"opacity": 0.02
-								},
-								"move": {
-									"direction": "right",
-									"speed": 0.05
-								},
-								"size": {
-									"value": 1
-								},
-								"opacity": {
-									"anim": {
-										"enable": true,
-										"speed": 1,
-										"opacity_min": 0.05
-									}
-								}
-							},
-							"interactivity": {
-								"events": {
-									"onclick": {
-										"enable": true,
-										"mode": "push"
-									}
-								},
-								"modes": {
-									"push": {
-										"particles_nb": 1
-									}
-								}
-							},
-							"retina_detect": true
-						}} />
 				</div>
 				<h1>
 					Hello, <br ></br>
