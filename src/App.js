8f6a1fab623f453f4a742ac58a7f7ce53b860075
Keegan H <keeganslane@gmail.com>
2019-05-31 18:52:04 -0700

    stashing

--- src/App.js
+++ src/App.js
@@ -11,49 +11,49 @@ function App() {
         <img src={logo} className="logo" alt="logo" />
         <div className="part">
         <Particles
-          params={{
-            "particles": {
-              "number": {
-                "value": 600,
-                "density": {
-                  "enable": true,
-                  "value_area": 1500
-                }
-              },
-              "line_linked": {
-                "enable": true,
-                "opacity": 0.02
-              },
-              "move": {
-                "direction": "right",
-                "speed": 0.05
-              },
-              "size": {
-                "value": 1
-              },
-              "opacity": {
-                "anim": {
-                  "enable": true,
-                  "speed": 1,
-                  "opacity_min": 0.05
-                }
-              }
-            },
-            "interactivity": {
-              "events": {
-                "onclick": {
-                  "enable": true,
-                  "mode": "push"
-                }
-              },
-              "modes": {
-                "push": {
-                  "particles_nb": 1
-                }
-              }
-            },
-            "retina_detect": true
-          }} />
+    params={{
+	    "particles": {
+	        "number": {
+	            "value": 60,
+	            "density": {
+	                "enable": true,
+	                "value_area": 1500
+	            }
+	        },
+	        "line_linked": {
+	            "enable": true,
+	            "opacity": 0.02
+	        },
+	        "move": {
+	            "direction": "right",
+	            "speed": 0.05
+	        },
+	        "size": {
+	            "value": 1
+	        },
+	        "opacity": {
+	            "anim": {
+	                "enable": true,
+	                "speed": 1,
+	                "opacity_min": 0.05
+	            }
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onclick": {
+	                "enable": true,
+	                "mode": "push"
+	            }
+	        },
+	        "modes": {
+	            "push": {
+	                "particles_nb": 1
+	            }
+	        }
+	    },
+	    "retina_detect": true
+	}} />
           </div>
         <h1>
           Hello, <br ></br>
