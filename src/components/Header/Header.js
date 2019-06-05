05fd81003279e1493c2dd98b2e1d1e387ffbab3a
keegan-h <keeganslane@gmail.com>
2019-06-04 20:27:33 -0700

    added styles back into skills compo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,30 +12,30 @@ function Header() {
 		hobbies: "music production, synthesizers, cyberpunk movies"
 	};
 
-    return (
-<header className="header">
-				<div className="part">
-				</div>
-				<h1>
-					Hello, <br ></br>
-					I'm {name.first}{" " + name.last}.
+	return (
+		<header className="header">
+			<div className="part">
+			</div>
+			<h1>
+				Hello, <br ></br>
+				I'm {name.first}{" " + name.last}.
         </h1>
-				<div className="sub">
-					I'm a full stack developer living in the Bay Area.
+			<div className="sub">
+				I'm a full stack developer living in the Bay Area.
         </div>
-				<div className="p">
-					Current Hobbies include:<br />
-					{aboutme.hobbies}
-				</div>
-				<a
-					className="link"
-					href="https://www.linkedin.com/in/keegan-helmbrecht/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					LinkedIn
+			<div className="p">
+				Current Hobbies include:<br />
+				{aboutme.hobbies}
+			</div>
+			<a
+				className="link"
+				href="https://www.linkedin.com/in/keegan-helmbrecht/"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				LinkedIn
         </a>
-            </header>
-    );
+		</header>
+	);
 }
 export default Header;
