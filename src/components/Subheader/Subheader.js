796ca4ab2f95ce943ccacd2e921477d5508ff743
Keegan H <keeganslane@gmail.com>
2019-06-06 11:59:06 -0700

    subheader comp broken out

---
+++ src/components/Subheader/Subheader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Subheader.css';
+
+function Subheader() {
+
+	return (
+			<div className="sub">
+				I'm a full stack developer living in the Bay Area.
+        </div>
+
+	);
+}
+export default Subheader;
