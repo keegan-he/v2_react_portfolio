921356cac5bcb48e37c1ddf31bf9787693371795
Keegan H <keeganslane@gmail.com>
2019-06-18 22:13:10 -0700

    additional routes created

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import About from './About';
+import Photography from './Photography';
+import Contact from './Contact';
+import Projects from './Projects';
 import { Switch, Route } from 'react-router-dom';
 
 const Main = () => (
     <Switch>
         <Route exact path ="/" component={null} />
         <Route path="/about" component={About} />
+        <Route path="/photography" component={Photography} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/projects" component={Projects} />
     </Switch>
 )
 
