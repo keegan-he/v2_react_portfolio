af543a84cb99ac5457ff5a92ba1623382fbf4d4c
Keegan H <keeganslane@gmail.com>
2019-06-18 22:37:36 -0700

    exported landing page to component in main

--- src/App.js
+++ src/App.js
@@ -33,28 +33,6 @@ function App() {
     </Layout>
 </div>
       <header className="header">
-        <div className="part">
-          </div>
-          
-        <h1>
-          Hello! <br ></br>
-          I'm Keegan Helmbrecht.
-        </h1>
-        <div className="sub">
-          I'm a full stack developer living in the Bay Area.
-        </div>
-        <div className="p">
-          Technical Expertise: <br ></br>
-          Python - React.js - JavaScript (ES6) - HTML / CSS - Django - Postgres SQL - Bash - Git - Pipenv - React Router - FlexBox & Grid - BEM - MVC - OOP - API engineering - ORM - OOP - Static Site Generation - SPA (Single Page Apps) - Webpack - Test Driven Development - Bootstrap - Agile
-        </div>
-        <a
-          className="link"
-          href="https://www.linkedin.com/in/keegan-helmbrecht/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
       </header>
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,12 @@ import About from './About';
 import Photography from './Photography';
 import Contact from './Contact';
 import Projects from './Projects';
+import Landing from './Landing';
 import { Switch, Route } from 'react-router-dom';
 
 const Main = () => (
     <Switch>
-        <Route exact path ="/" component={null} />
+        <Route exact path ="/" component={Landing} />
         <Route path="/about" component={About} />
         <Route path="/photography" component={Photography} />
         <Route path="/contact" component={Contact} />
