960dd269f13b31c2e943b1c608407c4c11d8738f
Keegan H <keeganslane@gmail.com>
2019-06-19 22:21:35 -0700

    playing with about format

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,19 @@ class About extends Component {
     render() {
         return(
             <div>
-            <h1> About section </h1>
+            <header className="header">
+            <h1>
+          </h1>
+          <div className="sub">
+          Throughout my life, I've had many varied passions and interests, usually centered around art and technology. I come from a family of creatives (painters, photographers, ceramicists, actors) so I've engaged in a number of artistic pursuits. In my school years, I worked at my local photo lab as a designer and printer. I've invested blood, sweat, and tears in my career as a dance music DJ and producer, which has spanned a good 17 years. In my spare time, I paint, hone my eye at photography and more recently, spend most of my time learning how to code. 
+        <br />
+        <br />
+          The other (equally intense) side of me has always loved computers. I went to my first Defcon as a 13 year old, I love Linux like an old friend and took that passion into studying Information Networking, Telecommunications, and Information Security. In between studying cisco networking protocols, I cut fish and managed a seafood department at a local grocery store in Seattle. 
+            <br />
+            <br />
+          Since the days of filleting and programming routers, I now work in the Bay Area, for the art e-commerce website, Redbubble, specifically in the field of Intellectual Property and Licensing, which has its own very interesting challenges. Working with digital content at Redbubble has proven to be a very engaging marriage between technology and art and I'd like to take these passions into the next stage of my career, as I transition into becoming a developer.
+          </div>
+          </header>
             </div>
         )
     }
